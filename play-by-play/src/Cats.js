256c18c44da40f186c25603e8e216a9ffd732bc9
Mindaugas Jacionis <m.jacionis@gmail.com>
2017-04-19 11:11:12 +0300

    Adds simple CRUD

--- play-by-play/src/Cats.js
+++ play-by-play/src/Cats.js
@@ -1,28 +1,35 @@
 import _ from 'lodash';
 
-class Cats {
-  const cats = [];
-  constructor(app) {
-    this.app = app;
-  }
+export default function(app) {
+  let cats = [];
 
-  // Create
-  post() {
-    app.post('/cat', (req, res) => {
-      cats.push(req.body);
-      res.json({ info: 'cat created successfully' });
-    });
-  }
+  /* Create */
+  app.post('/cat', (req, res) => {
+    cats.push(req.body);
+    res.json({ info: 'cat created successfully' });
+  });
 
-  // Read
-  get() {
-    app.get('/cat', (req, res) => {
-      res.send(cats)
-    });
-  }
+  /* Read */
+  app.get('/cat', (req, res) => {
+    res.send(cats)
+  });
 
-  // Update
-  //...
+  app.get('/cat/:id', (req, res) => {
+    res.send(
+      _.find(cats, { name: req.params.id })
+    );
+  });
+
+  /* Update */
+  app.put('/cat/:id', (req, res) => {
+    const index = _.findIndex(cats, { name: req.params.id });
+    _.merge(cats[index], req.body);
+    res.json({ info: 'cat updated successfully' });
+  });
+
+  /* Delete */
+  app.delete('/cat/:id', (req, res) => {
+    _.remove(cats, (cat) => cat.name === req.params.id);
+    res.json({ info: 'cat removed successfully' });
+  });
 }
-
-export default Cats;
--- play-by-play/src/index.js
+++ play-by-play/src/index.js
@@ -22,19 +22,16 @@
 // express lesson 2 | endpoints
 import express from 'express';
 import bodyParser from 'body-parser';
-import Cats from './Cats.js';
+import cats from './cats.js';
 
 const app = express();
-const cats = new Cats(app);
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencode({
+app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-app.get('/', (req, res) => {
-  // res.send('Hello Express World!');
-  res.json({ hello: 'express world' });
-}).listen(3000, () => {
+cats(app);
+
+const server = app.listen(3000, () => {
   console.log('Server running @ http://127.0.0.1:3000/');
 });
